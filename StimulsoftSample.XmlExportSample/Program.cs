ee00b3faf22ce6eb28cd53800b61f62408066fd1
Rodrigo Tessari <rodrigo.tessari@automatizatec.com.br>
2017-12-07 11:09:46 -0200

    add data sample

--- StimulsoftSample.XmlExportSample/Program.cs
+++ StimulsoftSample.XmlExportSample/Program.cs
@@ -1,15 +1,41 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Stimulsoft.Report;
+using Stimulsoft.Report.Export;
 
 namespace StimulsoftSample.XmlExportSample
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        [STAThread]
+        private static void Main(string[] args)
         {
+            StiOptions.Export.Xml.ConvertTagsToUpperCase = false;
+            StiOptions.Export.Xml.UseAliases = true;
+
+            var reportPath = "Reports\\Report.mrt";
+            StiReport report = new StiReport();
+            report.Load(reportPath);
+
+            var dataPath = "Data\\Demo";
+            var data = new DataSet("Demo");
+            data.ReadXmlSchema($"{dataPath}.xsd");
+            data.ReadXml($"{dataPath}.xml");
+            report.RegData(data);
+
+            //report.Design();
+            report.Render(false);
+
+            var pathTemp = "Temp\\sample.xml";
+            var settings = new StiXmlExportSettings();
+            report.ExportDocument(StiExportFormat.Xml, pathTemp, settings);
+
+            Process.Start(pathTemp);
         }
     }
 }
